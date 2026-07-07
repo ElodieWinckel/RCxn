9b213584bfc70d22f6e82520a8d5851cd970054b
ElodieWinckel <meuhlodi@hotmail.com>
2026-07-07 10:09:44 +0200

    Moved the graph-loading code for entries into a separate module.

--- app_entries/routes.py
+++ app_entries/routes.py
@@ -1,9 +1,23 @@
 from . import app_entries_blueprint
-import glob
 import re
-from flask import render_template, Response, url_for
-from rdflib import Graph, URIRef, Literal, Namespace, RDF, RDFS, FOAF, SKOS, DCTERMS
-import os
+from flask import render_template, Response
+from rdflib import Graph, URIRef, Literal, Namespace, RDF, RDFS, FOAF, SKOS
+
+# Load the graphs and namespaces defined in graph_loader.py
+from .graph_loader import (
+    g,
+    ont,
+    cx,
+    dc,
+    dcterm,
+    olia,
+    rcxn,
+    rsrch,
+    lg,
+    rd,
+    rdata,
+    compcon,
+)
 
 ###################################################
 ### FUNCTIONS
@@ -184,60 +198,6 @@ def find_compcon_uri_by_label(label_compcon) -> str:
         uri_compcon = ont.value(predicate=SKOS.altLabel, object=Literal(label_compcon))
     uri_compcon_no_prefix = re.sub(prefixes, "", str(uri_compcon))
     return uri_compcon_no_prefix
-
-###################################################
-### CREATE RDF GRAPH
-###################################################
-
-g = Graph()
-
-# Check if the production directory exists (otherwise, defaults to development directory)
-if os.path.exists("/data/www/RCxn"):
-    os.chdir("/data/www/RCxn")  # # Set the working directory to the application's production path
-
-else:
-    # Load and parse all RDF files from the folder with submissions (only during development process)
-    for ttl_file in glob.glob("instance/Submissions/**/*.ttl", recursive=True):
-        g.parse(ttl_file, format="turtle")
-
-# Load and parse all RDF files in the Abox
-for ttl_file in glob.glob("Abox/*.ttl"):
-    g.parse(ttl_file, format="turtle")
-
-# The following is for debug purposes: Read triples
-#for s, p, o in g:
-    #print(s, p, o)
-
-# Define the namespaces
-cx = Namespace("http://example.org/cx/")
-g.bind("cx", cx)
-dc = Namespace("http://purl.org/dc/elements/1.1/")
-g.bind("dc",dc)
-dcterm = Namespace("http://purl.org/dc/terms/")
-g.bind("dcterm",dcterm)
-olia = Namespace("http://purl.org/olia/olia.owl#")
-g.bind("olia", olia)
-rcxn = Namespace("https://bdlweb.phil.uni-erlangen.de/RCxn/ontologies/rcxn#")
-g.bind("rcxn", rcxn)
-rsrch = Namespace("https://bdlweb.phil.uni-erlangen.de/RCxn/ontologies/rsrch#")
-g.bind("rsrch", rsrch)
-lg = Namespace("https://bdlweb.phil.uni-erlangen.de/RCxn/ontologies/lg#")
-g.bind("lg", lg)
-rd = Namespace("http://example.org/rd/") #TODO: is this really the name?
-g.bind("rd", rd)
-rdata = Namespace("https://bdlweb.phil.uni-erlangen.de/RCxn/ontologies/rdata#") #TODO: is this really the name? create ontology
-g.bind("rdata", rdata)
-compcon = Namespace("https://bdlweb.phil.uni-erlangen.de/RCxn/ontologies/compcon#")
-g.bind("compcon", compcon)
-
-# Load the ontologies
-ont = Graph()
-for xlm_file in glob.glob("ontologies/*.rdf"):
-    ont.parse(xlm_file, format="xml")
-for xlm_file in glob.glob("ontologies/*.owl"): # for olia
-    ont.parse(xlm_file, format="xml")
-for ttl_file in glob.glob("ontologies/*.ttl"): # for compcon
-    ont.parse(ttl_file, format="turtle")
 
 ###################################################
 ### CREATE A LIST OF CONSTRUCTIONS
